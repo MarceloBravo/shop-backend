1688ea5310a162243e6fe4c4d83fc4fce38bffee
Marcelo Bravo <mabc@live.cl>
2025-07-10 16:05:44 -0400

    Se actualizó elnombre de la variable de entorno PWD por DB_PASS

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ import Sequelize from 'sequelize';
 
 const dbName = process.env.NODE_ENV === 'test' ? process.env.DB_TEST : process.env.DB;
 const dbUser = process.env.NODE_ENV === 'test' ? process.env.DB_USER : process.env.DB_USER;
-const dbPass = process.env.NODE_ENV === 'test' ? process.env.PWD : process.env.DB_PASS;
+const dbPass = process.env.NODE_ENV === 'test' ? process.env.DB_PASS : process.env.DB_PASS;
 const dbHost = process.env.NODE_ENV === 'test' ? process.env.HOST_TEST : process.env.HOST;
 const dbPort = process.env.NODE_ENV === 'test' ? process.env.PORT_TEST : process.env.PORT;
 
