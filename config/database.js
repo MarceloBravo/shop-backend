6fb59b4a9cf0add1df507834a5981460c405bbf3
Marcelo Bravo <mabc@live.cl>
2025-07-10 15:57:40 -0400

    Se actualizó el archivo config/database.js para estandarizar los nombreas de las variables

--- config/database.js
+++ config/database.js
@@ -1,14 +1,16 @@
 import dotenv from 'dotenv';
 dotenv.config({ path: '.env.test' });
-import Sequelize from 'sequelize'
+import Sequelize from 'sequelize';
 
-const conect = process.env.NODE_ENV === 'test' ? process.env.DB_TEST : process.env.DB
-const host = process.env.NODE_ENV === 'test' ? process.env.HOST_TEST : process.env.HOST
-const port = process.env.NODE_ENV === 'test' ? process.env.PORT_TEST : process.env.PORT
-//console.log('Conectando a la base de datos:', conect, host, port);
-export const sequelize = new Sequelize(conect,'postgres',process.env.PWD.toString(),{
-    host: host,
-    port: port,
+const dbName = process.env.NODE_ENV === 'test' ? process.env.DB_TEST : process.env.DB;
+const dbUser = process.env.NODE_ENV === 'test' ? process.env.DB_USER : process.env.DB_USER;
+const dbPass = process.env.NODE_ENV === 'test' ? process.env.PWD : process.env.DB_PASS;
+const dbHost = process.env.NODE_ENV === 'test' ? process.env.HOST_TEST : process.env.HOST;
+const dbPort = process.env.NODE_ENV === 'test' ? process.env.PORT_TEST : process.env.PORT;
+
+export const sequelize = new Sequelize(dbName, dbUser, dbPass, {
+    host: dbHost,
+    port: dbPort,
     dialect: 'postgres',
     logging: process.env.NODE_ENV !== 'test' ? console.log : false,
-})
+});
