1e5a61e95ae1265bc70e34c72b814117429941ff
Marcelo Bravo <mabc@live.cl>
2025-08-21 21:51:59 -0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json()); // Para procesar JSON
 app.use(morgan('dev'));
 
-app.set('host','0.0.0.0');
+app.set('host', (nodeEnv === 'production' ? '0.0.0.0' : '127.0.0.1'));
 const port = nodeEnv == 'test' ? process.env.APP_PORT_TEST : process.env.APP_PORT;
 app.set('port', port || 3000);
 
@@ -42,3 +42,4 @@ try{
 
 
 export default app;
+
